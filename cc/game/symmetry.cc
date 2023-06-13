894b4bb8afc1719204e08529f1e912f7fecc58b6
p3achyjr <anatolxliu@gmail.com>
2023-06-13 19:29:30 +0000

    add symmetry class

---
+++ cc/game/symmetry.cc
@@ -0,0 +1,63 @@
+#include "cc/game/symmetry.h"
+
+#include <array>
+#include <cstdint>
+#include <iostream>
+
+#include "cc/core/rand.h"
+
+namespace game {
+
+int Inv(int i, size_t grid_len) { return grid_len - i - 1; }
+
+int Rotate(int index, size_t grid_len, Rotation rot) {
+  int i = index / grid_len;
+  int j = index % grid_len;
+
+  switch (rot) {
+    case Rotation::k90:
+      return j * grid_len + Inv(i, grid_len);
+    case Rotation::k180:
+      return Inv(i, grid_len) * grid_len + Inv(j, grid_len);
+    case Rotation::k270:
+      return Inv(j, grid_len) * grid_len + i;
+  }
+
+  return index;
+}
+
+int Flip(int index, size_t grid_len) {
+  int i = index / grid_len;
+  int j = index % grid_len;
+
+  return i * grid_len + Inv(j, grid_len);
+}
+
+int TransformIndex(Symmetry sym, int index, size_t grid_len) {
+  switch (sym) {
+    case Symmetry::kIdentity:
+      return index;
+    case Symmetry::kRot90:
+      return Rotate(index, grid_len, Rotation::k90);
+    case Symmetry::kRot180:
+      return Rotate(index, grid_len, Rotation::k180);
+    case Symmetry::kRot270:
+      return Rotate(index, grid_len, Rotation::k270);
+    case Symmetry::kFlip:
+      return Flip(index, grid_len);
+    case Symmetry::kFlipRot90:
+      return Rotate(Flip(index, grid_len), grid_len, Rotation::k90);
+    case Symmetry::kFlipRot180:
+      return Rotate(Flip(index, grid_len), grid_len, Rotation::k180);
+    case Symmetry::kFlipRot270:
+      return Rotate(Flip(index, grid_len), grid_len, Rotation::k270);
+  }
+
+  return index;
+}
+
+Symmetry GetRandomSymmetry(core::PRng& prng) {
+  return static_cast<Symmetry>(core::RandRange(prng, 0, kSymUpperBound));
+}
+
+}  // namespace game
---
+++ cc/game/symmetry_test.cc
@@ -0,0 +1,66 @@
+#include "cc/game/symmetry.h"
+
+#include "cc/core/doctest_include.h"
+
+namespace game {
+
+static constexpr int kGridSize = 25;
+static constexpr int kGridLen = 5;
+
+TEST_CASE("BoardTest") {
+  // clang-format off
+  std::array<int, kGridSize> grid = {0,  1,  2,  3,  4,
+                                     5,  6,  7,  8,  9,
+                                     10, 11, 12, 13, 14,
+                                     15, 16, 17, 18, 19,
+                                     20, 21, 22, 23, 24};
+  std::array<int, kGridSize> grid_rot90 = {20, 15, 10, 5, 0,
+                                           21, 16, 11, 6, 1,
+                                           22, 17, 12, 7, 2,
+                                           23, 18, 13, 8, 3,
+                                           24, 19, 14, 9, 4};
+  std::array<int, kGridSize> grid_rot180 = {24, 23, 22, 21, 20,
+                                            19, 18, 17, 16, 15,
+                                            14, 13, 12, 11, 10,
+                                            9,  8,  7,  6,  5,
+                                            4,  3,  2,  1,  0};
+  std::array<int, kGridSize> grid_rot270 = {4, 9, 14, 19, 24,
+                                            3, 8, 13, 18, 23,
+                                            2, 7, 12, 17, 22,
+                                            1, 6, 11, 16, 21,
+                                            0, 5, 10, 15, 20};
+  std::array<int, kGridSize> grid_flip = {4,  3,  2,  1,  0,
+                                          9,  8,  7,  6,  5,
+                                          14, 13, 12, 11, 10,
+                                          19, 18, 17, 16, 15,
+                                          24, 23, 22, 21, 20};
+  std::array<int, kGridSize> grid_flip_rot90 = {24, 19, 14, 9, 4,
+                                                23, 18, 13, 8, 3,
+                                                22, 17, 12, 7, 2,
+                                                21, 16, 11, 6, 1,
+                                                20, 15, 10, 5, 0};
+  std::array<int, kGridSize> grid_flip_rot180 = {20, 21, 22, 23, 24,
+                                                 15, 16, 17, 18, 19,
+                                                 10, 11, 12, 13, 14,
+                                                 5,  6,  7,  8,  9,
+                                                 0,  1,  2,  3,  4};
+  std::array<int, kGridSize> grid_flip_rot270 = {0, 5, 10, 15, 20,
+                                                 1, 6, 11, 16, 21,
+                                                 2, 7, 12, 17, 22,
+                                                 3, 8, 13, 18, 23,
+                                                 4, 9, 14, 19, 24};
+  // clang-format on
+
+  CHECK(grid == ApplySymmetry(Symmetry::kIdentity, grid, kGridLen));
+  CHECK(grid_rot90 == ApplySymmetry(Symmetry::kRot90, grid, kGridLen));
+  CHECK(grid_rot180 == ApplySymmetry(Symmetry::kRot180, grid, kGridLen));
+  CHECK(grid_rot270 == ApplySymmetry(Symmetry::kRot270, grid, kGridLen));
+  CHECK(grid_flip == ApplySymmetry(Symmetry::kFlip, grid, kGridLen));
+  CHECK(grid_flip_rot90 == ApplySymmetry(Symmetry::kFlipRot90, grid, kGridLen));
+  CHECK(grid_flip_rot180 ==
+        ApplySymmetry(Symmetry::kFlipRot180, grid, kGridLen));
+  CHECK(grid_flip_rot270 ==
+        ApplySymmetry(Symmetry::kFlipRot270, grid, kGridLen));
+}
+
+}  // namespace game
